2873e46cf760d187ae2791bd2f6e654067f5970c
Patrick Robertson <pdnrobertson@gmail.com>
2018-03-18 19:17:40 +1100

    optimised production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
@@ -20,9 +23,10 @@ module.exports = {
       ]
     }]
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     historyApiFallback: true
   }
-};
+  }
+}
